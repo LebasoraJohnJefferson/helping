bafa1225963f743bc2fd5f48ae91fea772d6cac3
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-10-22 08:39:07 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app-routing.module';
+
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { NavComponent } from './component/nav/nav.component';
 
 import { AppComponent } from './app.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { HomeComponent } from './pages/home/home.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card'
+import {MatGridListModule} from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    ProfileComponent,
+    ContactComponent,
+    GalleryComponent,
+    NavComponent,
    
   ],
   imports: [
-    
+    MatGridListModule,
+    MatDividerModule,
+    MatButtonToggleModule,
     MatIconModule,
     MatToolbarModule,
     BrowserModule,
+    MatListModule,
+    AppRoutingModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
